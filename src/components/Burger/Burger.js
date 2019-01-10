8e25e3184d164a94f856a811fa943d1a02f5e3d5
davidmao8419 <davidmao8419@gmail.com>
2019-01-10 23:04:26 +0800

    output ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    //console.log(props.ingredients);
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey=>{
+        //console.log(Array(props.ingredients[igKey]));
+        //below use the spread operator to spread a new array I have to construct
+        return [...Array(props.ingredients[igKey])].map((_, i)=>{
+            return <BurgerIngredient key={igKey + i} type={igKey}/>
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'></BurgerIngredient>
-            <BurgerIngredient type='meat'></BurgerIngredient>
-            <BurgerIngredient type='cheese'></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom'></BurgerIngredient>
         </div>
     );
