dd532408932834a30974d5e606fe5302e270dbd7
davidmao8419 <davidmao8419@gmail.com>
2019-01-10 23:33:21 +0800

    output ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     //console.log(props.ingredients);
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey=>{
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey=>{
         //console.log(Array(props.ingredients[igKey]));
         //below use the spread operator to spread a new array I have to construct
         return [...Array(props.ingredients[igKey])].map((_, i)=>{
+            //console.log(igKey+i);
             return <BurgerIngredient key={igKey + i} type={igKey}/>
-        });
-    });
+        })
+    }).reduce((arr, el)=>{
+        //console.log(el);
+        return arr.concat(el);
+    }, []);
 
+    //console.log(transformedIngredients);
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding some ingredients!</p>
+    }
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'></BurgerIngredient>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,9 +6,9 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
             meat: 2
         }
     }
